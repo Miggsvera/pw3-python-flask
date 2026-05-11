9dd37f4b104617ff480e005cacaf5b1bf7a9d3c6
MIGUEL SILVA MARQUES PEREIRA <miguel.pereira@e239.local>
2026-05-11 14:05:12 -0300

    Chakablau

---
+++ aula-05.3-excluindo-cadastrando-dados-no-banco/controllers/routes.py
@@ -0,0 +1,108 @@
+# Importando o flask para a aplicação
+from flask import render_template, request, redirect, url_for
+# Importando o model de games
+from models.database import Game, db # do pacote do flask, importe a classe Flask
+
+
+
+# Criado a função principal para inicializar as rotas
+def init_app(app):
+    # VARIÁVEIS GLOBAIS
+    listaConsoles = ['Playstation 5','Xbox One','Super Nintendo','Atari1','3DS']
+    
+    listaGames = [{'titulo' : 'CS-GO', 'ano' : 2012, 'categoria' : 'FPS Online', 'plataforma' : 'PC (Windows)'}]
+    
+    # Criando a rota principal do site
+    @app.route('/')
+    # def cria funções no python
+    def home():
+        return render_template('index.html')
+
+    @app.route('/games')
+    def games():
+        #criando variáveis para a rota de games
+        titulo = "Portal 2"
+        ano = 2011
+        categoria = "Puzzle"
+        #Lista de Jogadores(uma lista é um vetor/array)
+        jogadores = ['Marcos','Richard','Miguel','Renato','Pedro']
+        
+        #Enviando categorias para html
+        return render_template('games.html',
+                            titulo = titulo,
+                            ano=ano,
+                            categoria=categoria,
+                            jogadores=jogadores)
+
+    @app.route('/consoles', methods=['GET', 'POST'])
+    def consoles():
+        # Criando um objeto
+        console = {"Nome": "Playstation 2",
+                "Fabricante": "Sony",
+                "Ano": 2000}
+        
+        
+        
+        #Recebendo o valor do formulário
+        if request.method == 'POST':
+            if request.form.get('novoConsole'):
+                listaConsoles.append(request.form.get('novoConsole'))
+        
+        return render_template('consoles.html',
+                            console= console,
+                            listaconsoles=listaConsoles)
+        
+    # Rota para cadastrar jogo
+    @app.route('/cadgames', methods=['GET', 'POST'])
+    def cadgames():
+        #Verificando se a requisição do usuario é do metodo post
+        # Recebendo os dados do formulário e enviando para página
+        if request.method == 'POST' :
+            listaGames.append({'titulo' : request.form.get('titulo'), 'ano' : request.form.get('ano'), 'categoria' : request.form.get('categoria'), 'plataforma' : request.form.get('plataforma')})
+            #Aqui o usuário será redirecionado novamente para a página
+            return redirect(url_for('cadgames'))
+            
+        return render_template('cadgames.html', 
+                               listaGames = listaGames)
+    
+    # Rota para o crud (estoque de jogos)
+    @app.route('/estoque', methods=['GET', 'POST'])
+    def estoque():
+        if request.method == 'POST':
+            # Criando novo jogo
+            titulo = request.form.get('titulo')
+            ano = int(request.form.get('ano'))
+            categoria = request.form.get('categoria')
+            plataforma = request.form.get('plataforma')
+            preco = float(request.form.get('preco'))
+            quantidade = int(request.form.get('quantidade'))
+            new_game = Game(titulo=titulo, ano=ano, categoria=categoria, plataforma=plataforma, preco=preco, quantidade=quantidade)
+            db.session.add(new_game)
+            db.session.commit()
+            return redirect(url_for('estoque'))
+        
+        # Selecionando todos os jogos da tabela
+        games = Game.query.all()
+        return render_template('estoque.html', games=games)
+    
+    @app.route('/estoque/delete/<int:id>')
+    def delete(id):
+        game = Game.query.get(id)
+        if game:
+            db.session.delete(game)
+            db.session.commit()
+        return redirect(url_for('estoque'))
+    
+    @app.route('/editar/<int:id>', methods=['GET', 'POST'])
+    def editar(id):
+        game = Game.query.get(id)
+        if request.method == 'POST':
+            game.titulo = request.form.get('titulo')
+            game.ano = int(request.form.get('ano'))
+            game.categoria = request.form.get('categoria')
+            game.plataforma = request.form.get('plataforma')
+            game.preco = float(request.form.get('preco'))
+            game.quantidade = int(request.form.get('quantidade'))
+            db.session.commit()
+            return redirect(url_for('estoque'))
+        return render_template('editGame.html', game=game)
